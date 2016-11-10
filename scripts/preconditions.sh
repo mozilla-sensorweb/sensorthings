8ce4431acdf1e1c41d482dfdbc6ae4e7a277b779
isabelrios <isabelrios@gmail.com>
2016-11-10 16:56:40 +0100

    trying to set the environment for OGC tests
    fixing travis file
    
    typo
    
    missing space
    
    fixing travis path
    
    fix select correct tag
    
    cannot find zip file
    
    fixing zip name
    
    errors in zip name
    
    fix configuring sta10 tests
    
    configure sta10 tests
    
    configure sta10 tests
    
    change mvn version
    
    need java 1.8
    
    remove mvn installation
    
    try to change to jdk 8
    
    back to node travis
    
    change order of executing tests
    
    removing unnecessary code
    
    unify all installations scripts
    
    fix files name using master
    
    files name
    
    common path and add v1.0 to form
    
    changes in path and adding script to populate db
    
    populate db when server ready
    
    adding preconditions
    
    only preconditios in db
    
    change form to level2
    
    level 3 no populate
    
    applying changes to run tests locally
    
    TE files name has changed
    
    make travis fails if ogc tests fail
    
    remove unnecessary line in travis

---
+++ scripts/preconditions.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+export HOST=http://localhost:8080/v1.0
+
+# CREATE PRECONDITIONS
+# These are needed to run tests under Conformance Level 2
+curl -X POST -H "Accept: application/json" -H "Content-Type: application/json"  -d '{
+    "description": "thing 1", 
+    "name": "thing name 1", 
+    "properties": { 
+        "reference": "first"
+    }, 
+    "Locations": [ { 
+        "description": "location 1", 
+        "name": "location name 1", 
+        "location": { 
+            "type": "Point", 
+            "coordinates": [ -117.05, 51.05 ]
+        }, 
+        "encodingType": "application/vnd.geo+json" 
+    } ], 
+    "Datastreams": [ { 
+        "unitOfMeasurement": { 
+            "name": "Lumen", 
+            "symbol": "lm", 
+            "definition": "http://www.qudt.org/qudt/owl/1.0.0/unit/Instances.html/Lumen" 
+        }, 
+        "description": "datastream 1", 
+        "name": "datastream name 1", 
+        "observationType": "http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_Measurement", 
+        "ObservedProperty": { 
+            "name": "Luminous Flux", 
+            "definition": "http://www.qudt.org/qudt/owl/1.0.0/quantity/Instances.html/LuminousFlux", 
+            "description": "observedProperty 1" 
+        }, 
+        "Sensor": { 
+            "description": "sensor 1", 
+            "name": "sensor name 1", 
+            "encodingType": "application/pdf", 
+            "metadata": "Light flux sensor" 
+        }, 
+        "Observations":[ { 
+            "phenomenonTime": "2015-03-03T00:00:00Z", 
+            "result": 3 
+        }, 
+        { 
+            "phenomenonTime": "2015-03-04T00:00:00Z", "result": 4 
+        } ] }, 
+        { 
+        "unitOfMeasurement": { 
+            "name": "Centigrade", 
+            "symbol": "C", 
+            "definition": "http://www.qudt.org/qudt/owl/1.0.0/unit/Instances.html/Lumen" 
+        }, 
+        "description": 
+            "datastream 2", 
+            "name": "datastream name 2", 
+            "observationType": "http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_Measurement", 
+            "ObservedProperty": { 
+                "name": "Tempretaure", 
+                "definition": "http://www.qudt.org/qudt/owl/1.0.0/quantity/Instances.html/Tempreture", 
+                "description": 
+                "observedProperty 2" 
+            }, 
+            "Sensor": { 
+                "description": "sensor 2", 
+                "name": "sensor name 2", 
+                "encodingType": "application/pdf", 
+                "metadata": "Tempreture sensor" 
+            }, 
+            "Observations":[ { 
+                "phenomenonTime": "2015-03-05T00:00:00Z", 
+                "result": 5 
+            }, 
+            { 
+                "phenomenonTime": "2015-03-06T00:00:00Z", 
+                "result": 6 
+            } ] 
+        } ] 
+
+}' "$HOST/Things"
