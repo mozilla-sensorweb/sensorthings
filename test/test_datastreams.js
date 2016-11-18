60d09c8ce479ddba4eb744ec1f9b71c68b647602
albertopq <albert.pastor@gmail.com>
2016-11-18 17:44:05 +0100

    [Issue #15] Moving obervationType from Observations to Datastreams (#86)

--- test/test_datastreams.js
+++ test/test_datastreams.js
@@ -1,14 +1,31 @@
 import commonTests  from './common';
 import {
-  datastreams
+  datastreams,
+  DatastreamsEntity
 } from './constants';
+
+import {
+  BAD_REQUEST,
+  ERRNO_VALIDATION_ERROR
+} from '../src/errors';
 
 const mandatory     = [
   'name',
   'description',
   'unitOfMeasurement',
-// Issue #15 'observationType'
+  'observationType'
 ];
 const optional      = ['phenomenonTime', 'resultTime'];
 
-commonTests(datastreams, 8880, mandatory, optional);
+commonTests(datastreams, 8880, mandatory, optional).then(tester => {
+  describe('Observations API - specific', () => {
+    describe('Check invalid observationType', () => {
+      it('should respond 400 if observationType is not valid', done => {
+        const body = Object.assign({}, DatastreamsEntity, {
+          observationType: 'random'
+        });
+        tester.postError(done, body, 400, ERRNO_VALIDATION_ERROR, BAD_REQUEST);
+      });
+    });
+  });
+});
--- test/test_observations.js
+++ test/test_observations.js
@@ -1,32 +1,14 @@
 import commonTests  from './common';
 import {
-  observations,
-  ObservationsEntity
+  observations
 } from './constants';
-
-import {
-  BAD_REQUEST,
-  ERRNO_VALIDATION_ERROR
-} from '../src/errors';
 
 const mandatory     = [
   'phenomenonTime',
   'result',
-  'resultTime',
-  'observationType'
+  'resultTime'
 ];
 // XXX #16 resultQuality
 const optional      = ['validTime', 'parameters'];
 
-commonTests(observations, 8885, mandatory, optional).then(tester => {
-  describe('Observations API - specific', () => {
-    describe('Check invalid observationType', () => {
-      it('should respond 400 if observationType is not valid', done => {
-        const body = Object.assign({}, ObservationsEntity, {
-          observationType: 'random'
-        });
-        tester.postError(done, body, 400, ERRNO_VALIDATION_ERROR, BAD_REQUEST);
-      });
-    });
-  });
-});
+commonTests(observations, 8885, mandatory, optional);
