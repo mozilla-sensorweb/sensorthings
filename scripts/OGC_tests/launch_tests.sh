d041ec3b91c6158d3dd906e38bad89c13ff12f34
isabelrios <isabelrios@gmail.com>
2016-11-23 15:05:25 +0100

    [Issue #96]Temporary solution to make travis fail

--- scripts/OGC_tests/launch_tests.sh
+++ scripts/OGC_tests/launch_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+echo $PWD
 COMMON_PATH=$PWD
 
 echo "Launching SensorThings Test Suite tests"
@@ -8,3 +9,6 @@ echo "Launching SensorThings Test Suite tests"
 # Path to te-install, TE_BASE and forms are needed
 # Using relative path
 $COMMON_PATH/test/TE_BASE/te-install/bin/unix/test.sh -source=sta10/1.0/ctl/sta10-suite.ctl -form=$COMMON_PATH/test/forms/sta10.xml
+
+# Check if there has been failures running OGC tests
+source ./scripts/OGC_tests/check_tests_failures.sh
