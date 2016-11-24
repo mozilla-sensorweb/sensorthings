38a5014c7a835d58e4b346011175c3c92f504f7a
isabelrios <isabelrios@gmail.com>
2016-11-24 17:01:07 +0100

    change console-teamengine files

--- scripts/OGC_tests/install_tests.sh
+++ scripts/OGC_tests/install_tests.sh
@@ -8,8 +8,10 @@ git clone https://github.com/opengeospatial/teamengine.git test/teamengine
 
 cd $BASE_PATH/test/teamengine
 
-# Using master branch
+# Using stable branch
 echo "Building TE source in $PWD"
+git checkout 4.10
+
 mvn install
 
 echo "Preparing TE_BASE"
@@ -22,8 +24,8 @@ cd $TE_BASE
 echo "Changed dir to $PWD"
 
 # Copying teamengine-console-xxxx-base.zip into TE_BASE
-cp $BASE_PATH/test/teamengine/teamengine-console/target/teamengine-console-4.10-RC2-SNAPSHOT-base.zip .
-unzip teamengine-console-4.10-RC2-SNAPSHOT-base.zip
+cp $BASE_PATH/test/teamengine/teamengine-console/target/teamengine-console-4.10-base.zip .
+unzip teamengine-console-4.10-base.zip
 
 echo "Preparing te-install"
 mkdir $TE_BASE/te-install
@@ -32,8 +34,8 @@ cd te-install
 echo $PWD
 
 # Copying teamengine-console-xxxx-bin.zip into TE_BASE
-cp $BASE_PATH/test/teamengine/teamengine-console/target/teamengine-console-4.10-RC2-SNAPSHOT-bin.zip .
-unzip teamengine-console-4.10-RC2-SNAPSHOT-bin.zip
+cp $BASE_PATH/test/teamengine/teamengine-console/target/teamengine-console-4.10-bin.zip .
+unzip teamengine-console-4.10-bin.zip
 
 cd ..
 
