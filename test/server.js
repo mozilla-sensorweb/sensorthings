77b44b691907733277c38ace644cce9af849024d
Russ Nicoletti <rnicoletti@mozilla.com>
2017-01-31 06:40:06 -0800

    [Issue #228] consumers to add model hooks (#235)

--- test/server.js
+++ test/server.js
@@ -7,16 +7,45 @@ let app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+let hooks = {};
 const config = {
   db: {
     host: 'localhost',
     port: 5432,
     name: 'sensorthingstest',
     user: 'postgres',
-    pass: '12345678'
+    pass: '12345678',
+    hooks: {
+        beforeCreate: () => {
+          hooks.beforeCreate = true
+        },
+        afterCreate: () => {
+          hooks.afterCreate = true
+        },
+        beforeDestroy: () => {
+          hooks.beforeDestroy = true
+        },
+        afterDestroy: () => {
+          hooks.afterDestroy = true
+        },
+        beforeUpdate: () => {
+          hooks.beforeUpdate = true
+        },
+        afterUpdate: () => {
+          hooks.afterUpdate = true
+        }
+    }
   }
-};
+}
 
 app.use('/', SensorThings(config));
 
-exports = module.exports = app;
+export default app;
+
+exports.resetHooks = () => {
+  hooks = {}
+};
+
+exports.hookInvoked = name => {
+  return hooks[name] !== undefined
+};
